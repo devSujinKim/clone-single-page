abf961e280be7e0600d4aab873df9ad4933b082c
jina <kimsujin0425@naver.com>
2021-08-28 18:54:47 +0900

    header 컴포넌트 위치 이동

--- src/containers/index/_comps/Main/index.js
+++ src/containers/index/_comps/Main/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { css, keyframes } from '@emotion/react';
-import Header from 'components/Header';
+import Header from 'components/Layout/Header';
 
 const mainContainer = css`
   width: 100%;
