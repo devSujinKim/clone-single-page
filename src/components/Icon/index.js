6b83519421977b248a3280f194e7ebe1a072b1da
jina <kimsujin0425@naver.com>
2021-08-28 16:38:37 +0900

    icon 추가

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,7 +1,21 @@
 import dynamic from 'next/dynamic';
 
+const AlwaysIcon = dynamic(() => import('./components/AlwaysIcon'));
+const EarthIcon = dynamic(() => import('./components/EarthIcon'));
 const LogoIcon = dynamic(() => import('./components/LogoIcon'));
 const MailIcon = dynamic(() => import('./components/MailIcon'));
+const PercentIcon = dynamic(() => import('./components/PercentIcon'));
+const ProtectIcon = dynamic(() => import('./components/ProtectIcon'));
 const SearchIcon = dynamic(() => import('./components/SearchIcon'));
+const ShippingIcon = dynamic(() => import('./components/ShippingIcon'));
 
-export { LogoIcon, MailIcon, SearchIcon };
+export {
+  AlwaysIcon,
+  EarthIcon,
+  LogoIcon,
+  MailIcon,
+  PercentIcon,
+  ProtectIcon,
+  SearchIcon,
+  ShippingIcon,
+};
